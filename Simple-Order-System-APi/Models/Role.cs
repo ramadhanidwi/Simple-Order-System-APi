b7fd3e323f7bccee8a2af95195081eb5c705aecd
Ramadhani <ramadhanidwin@gmail.com>
2023-03-07 21:44:26 +0700

    Create Role.cs

---
+++ Simple-Order-System-APi/Models/Role.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Simple_Order_System_APi.Models
+{
+    [Table("tb_m_rma_role")]
+    public class Role
+    {
+        [Key, Column("id", TypeName ="nchar(11)")]
+        public int id { get; set; }
+
+        [Required, Column("name", TypeName ="nvarchar(50)")]
+        public string name { get; set; }
+    }
+}
